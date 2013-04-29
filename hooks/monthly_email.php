e39d7c34d5da50623dfaf897c996bac0e69e1045
Dave Oster <daveosterjr@gmail.com>
2013-04-29 09:55:04 -0500

    Added cronjob

---
+++ hooks/monthly_email.php
@@ -0,0 +1,24 @@
+<?php
+	
+	include("../config.php");
+	include("../scripts/dbconnect.php");
+	include("../scripts/contact.php");
+	
+	$data = mysql_query("SELECT email FROM users")or die(mysql_error());
+
+	while($info = mysql_fetch_array($data)){
+		
+		$user_email = $info['email'];
+		$firstname = $info['firstname'];
+		$lastname = $info['lastname'];
+		
+		$user_subject = 'Rembr.it Weekly Reminder';
+		$user_body = "<h1>Rembr.it Weekly Reminder</h1>
+                		<p>Rembr.it Weekly Reminder for ".$firstname." ".$lastname.".</p>               	
+                		<p>Log in to <a href='http://rembr.it'>Rembr.it</a> and Update us on your child.</p>
+                		<p>-Rembr.it Team</p>";
+		contact($user_subject, $user_body, $user_email);
+		
+	}
+
+?>
